0bae9325ef0d3afea3626a77d3468b76a24c557c
Hyeun9991 <hyeun9991@gmail.com>
2023-04-24 15:52:18 +0900

    user 스키마, 모델 생성

--- boiler-plate/src/app.ts
+++ boiler-plate/src/app.ts
@@ -3,6 +3,18 @@ import express, { Request, Response, NextFunction } from 'express';
 const app = express();
 const port = 8080;
 
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://hyeun9991:abcd1234@boilerplate.sugnrkx.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('MongoDB Connected...!');
+  })
+  .catch((err: Error) => {
+    console.log(err);
+  });
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('welcome! 안녕');
 });
