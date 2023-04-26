45521cd2baa8c7b9e6aa459a505e1c0670da8247
Hyeun9991 <hyeun9991@gmail.com>
2023-04-26 22:13:24 +0900

    비밀 정보 보호

--- boiler-plate/src/app.ts
+++ boiler-plate/src/app.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express';
+import User from '../models/User';
 
 const app = express();
+const mongoose = require('mongoose');
+const config = require('../config/key.js');
 const port = 8080;
 
-const mongoose = require('mongoose');
+// 데이터 분석
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// MongoDB와 어플리케이션 연결
 mongoose
-  .connect(
-    'mongodb+srv://hyeun9991:abcd1234@boilerplate.sugnrkx.mongodb.net/?retryWrites=true&w=majority'
-  )
+  .connect(config.mongoURI)
   .then(() => {
     console.log('MongoDB Connected...!');
   })
@@ -16,7 +21,28 @@ mongoose
   });
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('welcome! 안녕');
+  res.send('welcome!');
+});
+
+// 회원가입에 필요한 정보들을 client에서 가져오고, DB에 저장
+app.post('/register', async (req: Request, res: Response) => {
+  const user = new User(req.body);
+
+  await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err: Error) => {
+      console.log(err);
+
+      res.json({
+        success: false,
+        err: err,
+      });
+    });
 });
 
 app.listen(port, () => {
